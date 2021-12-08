1be08833f3824757b02597ba509a65a423fdbf1e
zhaochaofan <zhaochaofan@gyyx.cn>
2021-12-08 15:11:32 +0800

    更新：Editor中纯数据模式下绘制Entity的LocalToWorld的自身坐标系

--- PureMVCFramework/Entity/Transform/TRSToLocalToWorldSystem.cs
+++ PureMVCFramework/Entity/Transform/TRSToLocalToWorldSystem.cs
@@ -8,6 +8,15 @@ namespace PureMVCFramework.Entity
         protected override void OnUpdate(int index, Entity entity, LocalToWorld c1, Position c2, Rotation c3, Scale c4)
         {
             c1.Value = Matrix4x4.TRS(c2.Value, math.normalize(c3.Value), c4.Value);
+
+#if UNITY_EDITOR
+            if (EntityManager.Instance.IsDataMode)
+            {
+                Debug.DrawLine(c1.Position, c1.Position + math.mul(c1.Rotation, math.right()) * 1, Color.red);
+                Debug.DrawLine(c1.Position, c1.Position + math.mul(c1.Rotation, math.up()) * 1, Color.green);
+                Debug.DrawLine(c1.Position, c1.Position + math.mul(c1.Rotation, math.forward()) * 1, Color.blue);
+            }
+#endif
         }
     }
 }
