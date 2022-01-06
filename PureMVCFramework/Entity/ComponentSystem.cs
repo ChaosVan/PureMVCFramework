32a094bdc38f4b4aa2a0adf37017e0b4d4e251ca
zhaochaofan <zhaochaofan@gyyx.cn>
2022-01-06 14:39:38 +0800

    更新：重新生成System代码

--- PureMVCFramework/Entity/ComponentSystem.cs
+++ PureMVCFramework/Entity/ComponentSystem.cs
@@ -16,7 +16,7 @@ namespace PureMVCFramework.Entity
 		public override void OnInitialized(params object[] args)
 		{
 			base.OnInitialized(args);
-            hash1 = Entity.StringToHash(typeof(T1).FullName);
+			hash1 = Entity.StringToHash(typeof(T1).FullName);
 		}
 		public override void OnRecycle()
 		{
@@ -26,30 +26,28 @@ namespace PureMVCFramework.Entity
 		public sealed override void InjectEntity(Entity entity)
 		{
 			IComponent[] c = new IComponent[1];
-
-            bool tf = entity.components.TryGetValue(hash1, out c[0]);
-
-            if (Entities.Contains(entity))
-            {
-                if (!tf)
-                {
-                    var i = Entities.IndexOf(entity);
-                    Entities.RemoveAt(i);
-                    Components1.RemoveAt(i);
-                }
-            }
-            else if (tf)
-            {
-                Entities.Add(entity);
-                Components1.Add((T1)c[0]);
-            }
+			bool tf = entity.components.TryGetValue(hash1, out c[0]);
+			if (Entities.Contains(entity))
+			{
+				if (!tf)
+				{
+					var i = Entities.IndexOf(entity);
+					Entities.RemoveAt(i);
+					Components1.RemoveAt(i);
+				}
+			}
+			else if (tf)
+			{
+				Entities.Add(entity);
+				Components1.Add((T1)c[0]);
+			}
 		}
 		public sealed override void Update()
 		{
 			for (int i = 0; i < Entities.Count; ++i)
-            {
-                OnUpdate(i, Entities[i], Components1[i]);
-            }
+			{
+				OnUpdate(i, Entities[i], Components1[i]);
+			}
 		}
 		protected abstract void OnUpdate(int index, Entity entity, T1 component1);
 	}
@@ -68,7 +66,7 @@ namespace PureMVCFramework.Entity
 		public override void OnInitialized(params object[] args)
 		{
 			base.OnInitialized(args);
-            hash1 = Entity.StringToHash(typeof(T1).FullName);
+			hash1 = Entity.StringToHash(typeof(T1).FullName);
 			hash2 = Entity.StringToHash(typeof(T2).FullName);
 		}
 		public override void OnRecycle()
@@ -80,33 +78,30 @@ namespace PureMVCFramework.Entity
 		public sealed override void InjectEntity(Entity entity)
 		{
 			IComponent[] c = new IComponent[2];
-
-            bool tf = entity.components.TryGetValue(hash1, out c[0])
-				 && entity.components.TryGetValue(hash2, out c[1]);
-
-            if (Entities.Contains(entity))
-            {
-                if (!tf)
-                {
-                    var i = Entities.IndexOf(entity);
-                    Entities.RemoveAt(i);
-                    Components1.RemoveAt(i);
+			bool tf = entity.components.TryGetValue(hash1, out c[0]) && entity.components.TryGetValue(hash2, out c[1]);
+			if (Entities.Contains(entity))
+			{
+				if (!tf)
+				{
+					var i = Entities.IndexOf(entity);
+					Entities.RemoveAt(i);
+					Components1.RemoveAt(i);
 					Components2.RemoveAt(i);
-                }
-            }
-            else if (tf)
-            {
-                Entities.Add(entity);
-                Components1.Add((T1)c[0]);
+				}
+			}
+			else if (tf)
+			{
+				Entities.Add(entity);
+				Components1.Add((T1)c[0]);
 				Components2.Add((T2)c[1]);
-            }
+			}
 		}
 		public sealed override void Update()
 		{
 			for (int i = 0; i < Entities.Count; ++i)
-            {
-                OnUpdate(i, Entities[i], Components1[i], Components2[i]);
-            }
+			{
+				OnUpdate(i, Entities[i], Components1[i], Components2[i]);
+			}
 		}
 		protected abstract void OnUpdate(int index, Entity entity, T1 component1, T2 component2);
 	}
@@ -130,7 +125,7 @@ namespace PureMVCFramework.Entity
 		public override void OnInitialized(params object[] args)
 		{
 			base.OnInitialized(args);
-            hash1 = Entity.StringToHash(typeof(T1).FullName);
+			hash1 = Entity.StringToHash(typeof(T1).FullName);
 			hash2 = Entity.StringToHash(typeof(T2).FullName);
 			hash3 = Entity.StringToHash(typeof(T3).FullName);
 		}
@@ -144,36 +139,32 @@ namespace PureMVCFramework.Entity
 		public sealed override void InjectEntity(Entity entity)
 		{
 			IComponent[] c = new IComponent[3];
-
-            bool tf = entity.components.TryGetValue(hash1, out c[0])
-				 && entity.components.TryGetValue(hash2, out c[1])
-				 && entity.components.TryGetValue(hash3, out c[2]);
-
-            if (Entities.Contains(entity))
-            {
-                if (!tf)
-                {
-                    var i = Entities.IndexOf(entity);
-                    Entities.RemoveAt(i);
-                    Components1.RemoveAt(i);
+			bool tf = entity.components.TryGetValue(hash1, out c[0]) && entity.components.TryGetValue(hash2, out c[1]) && entity.components.TryGetValue(hash3, out c[2]);
+			if (Entities.Contains(entity))
+			{
+				if (!tf)
+				{
+					var i = Entities.IndexOf(entity);
+					Entities.RemoveAt(i);
+					Components1.RemoveAt(i);
 					Components2.RemoveAt(i);
 					Components3.RemoveAt(i);
-                }
-            }
-            else if (tf)
-            {
-                Entities.Add(entity);
-                Components1.Add((T1)c[0]);
+				}
+			}
+			else if (tf)
+			{
+				Entities.Add(entity);
+				Components1.Add((T1)c[0]);
 				Components2.Add((T2)c[1]);
 				Components3.Add((T3)c[2]);
-            }
+			}
 		}
 		public sealed override void Update()
 		{
 			for (int i = 0; i < Entities.Count; ++i)
-            {
-                OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i]);
-            }
+			{
+				OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i]);
+			}
 		}
 		protected abstract void OnUpdate(int index, Entity entity, T1 component1, T2 component2, T3 component3);
 	}
@@ -202,7 +193,7 @@ namespace PureMVCFramework.Entity
 		public override void OnInitialized(params object[] args)
 		{
 			base.OnInitialized(args);
-            hash1 = Entity.StringToHash(typeof(T1).FullName);
+			hash1 = Entity.StringToHash(typeof(T1).FullName);
 			hash2 = Entity.StringToHash(typeof(T2).FullName);
 			hash3 = Entity.StringToHash(typeof(T3).FullName);
 			hash4 = Entity.StringToHash(typeof(T4).FullName);
@@ -218,39 +209,34 @@ namespace PureMVCFramework.Entity
 		public sealed override void InjectEntity(Entity entity)
 		{
 			IComponent[] c = new IComponent[4];
-
-            bool tf = entity.components.TryGetValue(hash1, out c[0])
-				 && entity.components.TryGetValue(hash2, out c[1])
-				 && entity.components.TryGetValue(hash3, out c[2])
-				 && entity.components.TryGetValue(hash4, out c[3]);
-
-            if (Entities.Contains(entity))
-            {
-                if (!tf)
-                {
-                    var i = Entities.IndexOf(entity);
-                    Entities.RemoveAt(i);
-                    Components1.RemoveAt(i);
+			bool tf = entity.components.TryGetValue(hash1, out c[0]) && entity.components.TryGetValue(hash2, out c[1]) && entity.components.TryGetValue(hash3, out c[2]) && entity.components.TryGetValue(hash4, out c[3]);
+			if (Entities.Contains(entity))
+			{
+				if (!tf)
+				{
+					var i = Entities.IndexOf(entity);
+					Entities.RemoveAt(i);
+					Components1.RemoveAt(i);
 					Components2.RemoveAt(i);
 					Components3.RemoveAt(i);
 					Components4.RemoveAt(i);
-                }
-            }
-            else if (tf)
-            {
-                Entities.Add(entity);
-                Components1.Add((T1)c[0]);
+				}
+			}
+			else if (tf)
+			{
+				Entities.Add(entity);
+				Components1.Add((T1)c[0]);
 				Components2.Add((T2)c[1]);
 				Components3.Add((T3)c[2]);
 				Components4.Add((T4)c[3]);
-            }
+			}
 		}
 		public sealed override void Update()
 		{
 			for (int i = 0; i < Entities.Count; ++i)
-            {
-                OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i], Components4[i]);
-            }
+			{
+				OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i], Components4[i]);
+			}
 		}
 		protected abstract void OnUpdate(int index, Entity entity, T1 component1, T2 component2, T3 component3, T4 component4);
 	}
@@ -284,7 +270,7 @@ namespace PureMVCFramework.Entity
 		public override void OnInitialized(params object[] args)
 		{
 			base.OnInitialized(args);
-            hash1 = Entity.StringToHash(typeof(T1).FullName);
+			hash1 = Entity.StringToHash(typeof(T1).FullName);
 			hash2 = Entity.StringToHash(typeof(T2).FullName);
 			hash3 = Entity.StringToHash(typeof(T3).FullName);
 			hash4 = Entity.StringToHash(typeof(T4).FullName);
@@ -302,42 +288,36 @@ namespace PureMVCFramework.Entity
 		public sealed override void InjectEntity(Entity entity)
 		{
 			IComponent[] c = new IComponent[5];
-
-            bool tf = entity.components.TryGetValue(hash1, out c[0])
-				 && entity.components.TryGetValue(hash2, out c[1])
-				 && entity.components.TryGetValue(hash3, out c[2])
-				 && entity.components.TryGetValue(hash4, out c[3])
-				 && entity.components.TryGetValue(hash5, out c[4]);
-
-            if (Entities.Contains(entity))
-            {
-                if (!tf)
-                {
-                    var i = Entities.IndexOf(entity);
-                    Entities.RemoveAt(i);
-                    Components1.RemoveAt(i);
+			bool tf = entity.components.TryGetValue(hash1, out c[0]) && entity.components.TryGetValue(hash2, out c[1]) && entity.components.TryGetValue(hash3, out c[2]) && entity.components.TryGetValue(hash4, out c[3]) && entity.components.TryGetValue(hash5, out c[4]);
+			if (Entities.Contains(entity))
+			{
+				if (!tf)
+				{
+					var i = Entities.IndexOf(entity);
+					Entities.RemoveAt(i);
+					Components1.RemoveAt(i);
 					Components2.RemoveAt(i);
 					Components3.RemoveAt(i);
 					Components4.RemoveAt(i);
 					Components5.RemoveAt(i);
-                }
-            }
-            else if (tf)
-            {
-                Entities.Add(entity);
-                Components1.Add((T1)c[0]);
+				}
+			}
+			else if (tf)
+			{
+				Entities.Add(entity);
+				Components1.Add((T1)c[0]);
 				Components2.Add((T2)c[1]);
 				Components3.Add((T3)c[2]);
 				Components4.Add((T4)c[3]);
 				Components5.Add((T5)c[4]);
-            }
+			}
 		}
 		public sealed override void Update()
 		{
 			for (int i = 0; i < Entities.Count; ++i)
-            {
-                OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i], Components4[i], Components5[i]);
-            }
+			{
+				OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i], Components4[i], Components5[i]);
+			}
 		}
 		protected abstract void OnUpdate(int index, Entity entity, T1 component1, T2 component2, T3 component3, T4 component4, T5 component5);
 	}
@@ -376,7 +356,7 @@ namespace PureMVCFramework.Entity
 		public override void OnInitialized(params object[] args)
 		{
 			base.OnInitialized(args);
-            hash1 = Entity.StringToHash(typeof(T1).FullName);
+			hash1 = Entity.StringToHash(typeof(T1).FullName);
 			hash2 = Entity.StringToHash(typeof(T2).FullName);
 			hash3 = Entity.StringToHash(typeof(T3).FullName);
 			hash4 = Entity.StringToHash(typeof(T4).FullName);
@@ -396,45 +376,38 @@ namespace PureMVCFramework.Entity
 		public sealed override void InjectEntity(Entity entity)
 		{
 			IComponent[] c = new IComponent[6];
-
-            bool tf = entity.components.TryGetValue(hash1, out c[0])
-				 && entity.components.TryGetValue(hash2, out c[1])
-				 && entity.components.TryGetValue(hash3, out c[2])
-				 && entity.components.TryGetValue(hash4, out c[3])
-				 && entity.components.TryGetValue(hash5, out c[4])
-				 && entity.components.TryGetValue(hash6, out c[5]);
-
-            if (Entities.Contains(entity))
-            {
-                if (!tf)
-                {
-                    var i = Entities.IndexOf(entity);
-                    Entities.RemoveAt(i);
-                    Components1.RemoveAt(i);
+			bool tf = entity.components.TryGetValue(hash1, out c[0]) && entity.components.TryGetValue(hash2, out c[1]) && entity.components.TryGetValue(hash3, out c[2]) && entity.components.TryGetValue(hash4, out c[3]) && entity.components.TryGetValue(hash5, out c[4]) && entity.components.TryGetValue(hash6, out c[5]);
+			if (Entities.Contains(entity))
+			{
+				if (!tf)
+				{
+					var i = Entities.IndexOf(entity);
+					Entities.RemoveAt(i);
+					Components1.RemoveAt(i);
 					Components2.RemoveAt(i);
 					Components3.RemoveAt(i);
 					Components4.RemoveAt(i);
 					Components5.RemoveAt(i);
 					Components6.RemoveAt(i);
-                }
-            }
-            else if (tf)
-            {
-                Entities.Add(entity);
-                Components1.Add((T1)c[0]);
+				}
+			}
+			else if (tf)
+			{
+				Entities.Add(entity);
+				Components1.Add((T1)c[0]);
 				Components2.Add((T2)c[1]);
 				Components3.Add((T3)c[2]);
 				Components4.Add((T4)c[3]);
 				Components5.Add((T5)c[4]);
 				Components6.Add((T6)c[5]);
-            }
+			}
 		}
 		public sealed override void Update()
 		{
 			for (int i = 0; i < Entities.Count; ++i)
-            {
-                OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i], Components4[i], Components5[i], Components6[i]);
-            }
+			{
+				OnUpdate(i, Entities[i], Components1[i], Components2[i], Components3[i], Components4[i], Components5[i], Components6[i]);
+			}
 		}
 		protected abstract void OnUpdate(int index, Entity entity, T1 component1, T2 component2, T3 component3, T4 component4, T5 component5, T6 component6);
 	}
