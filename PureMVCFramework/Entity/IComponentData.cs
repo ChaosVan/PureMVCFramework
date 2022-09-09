2f8da6aad80f1bec14692c2f5edcab2f2e49e8d1
zhaochaofan <zhaochaofan@gyyx.cn>
2022-09-09 18:09:27 +0800

    更新：增加两个泛型的ComponentWrapper

--- PureMVCFramework/Entity/IComponentData.cs
+++ PureMVCFramework/Entity/IComponentData.cs
@@ -13,7 +13,7 @@ namespace PureMVCFramework.Entity
     {
     }
 
-    public class ComponentWrapper<T> : MonoBehaviour where T : IComponentData, new()
+    public class ComponentWrapper<T> : MonoBehaviour where T : IComponentData
     {
         private bool started;
         protected T self;
@@ -23,6 +23,33 @@ namespace PureMVCFramework.Entity
         {
             entity = EntityManager.Create(gameObject);
             self = entity.GetOrAddComponentData<T>();
+            started = true;
+        }
+
+        private void Update()
+        {
+            if (!started)
+                Start();
+        }
+
+        private void OnDisable()
+        {
+            started = false;
+        }
+    }
+
+    public class ComponentWrapper<T1, T2> : MonoBehaviour where T1: IComponentData where T2 : IComponentData
+    {
+        private bool started;
+        protected T1 t1;
+        protected T2 t2;
+        protected Entity entity;
+
+        protected virtual void Start()
+        {
+            entity = EntityManager.Create(gameObject);
+            t1 = entity.GetOrAddComponentData<T1>();
+            t2 = entity.GetOrAddComponentData<T2>();
             started = true;
         }
 
