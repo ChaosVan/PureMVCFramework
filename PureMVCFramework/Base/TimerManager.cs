b9b1d9baaf9139c43cde7bc4ed74a42f34a0274e
zhaochaofan <zhaochaofan@gyyx.cn>
2022-11-18 11:51:32 +0800

    修复：Timer回收的问题

--- PureMVCFramework/Base/TimerManager.cs
+++ PureMVCFramework/Base/TimerManager.cs
@@ -8,115 +8,27 @@ using UnityEngine;
 
 namespace PureMVCFramework
 {
+    public interface ITimerTask
+    {
+        bool Update(float deltaTime);
+        bool Execute();
+        void Stop();
+    }
+
+    public enum TimerType
+    {
+        FIXED_DURATION,
+        FIXED_REALTIME_DURATION,
+        EVERY_FRAME,
+    }
+
     public class TimerManager : SingletonBehaviour<TimerManager>
     {
-        public enum TimerType
-        {
-            FIXED_DURATION,
-            FIXED_REALTIME_DURATION,
-            EVERY_FRAME,
-        }
-
-        public class TimerTask : IDisposable
-        {
-            public string name;
-
-            public TimerType timerType;
-            public float startTime;
-            public float interval;
-            public int repeatTimes;
-            public Func<bool> executable;
-
-            private int executeTimes;
-            private float elapsedTime;
-            private float nextExecuteTime;
-            private bool started;
-            private bool stopped;
-
-            public bool IsStopped { get => stopped; }
-
-#if ODIN_INSPECTOR
-            [ShowInInspector]
-#endif
-            public float ElapsedTime => elapsedTime;
-
-            public void Stop()
-            {
-                stopped = true;
-            }
-
-            public void Dispose()
-            {
-                executeTimes = 0;
-                elapsedTime = 0;
-                nextExecuteTime = 0;
-                started = false;
-                stopped = false;
-            }
-
-            internal bool Update(float delta)
-            {
-                //if (timerType == TimerType.FIXED_DURATION)
-                //    elapsedTime += delta;
-
-                elapsedTime += delta;
-
-                float time = Time.time;
-                if (timerType == TimerType.FIXED_REALTIME_DURATION)
-                    time = Time.realtimeSinceStartup;
-
-                if (!started)
-                {
-                    if (time >= startTime)
-                    {
-                        started = true;
-                        nextExecuteTime = startTime + interval;
-                        return true;
-                    }
-                }
-                else
-                {
-                    if (timerType == TimerType.EVERY_FRAME)
-                        return true;
-
-                    if (time >= nextExecuteTime)
-                    {
-                        nextExecuteTime += interval;
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-            /// <summary>
-            /// Execute this instance.
-            /// </summary>
-            /// <returns>if <see langword="true"/>, remove this task</returns>
-            internal bool Execute()
-            {
-                executeTimes++;
-                if (executable != null)
-                {
-                    try
-                    {
-                        return stopped || executable() || executeTimes == repeatTimes;
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.LogError(e);
-                    }
-                }
-
-                return true;
-            }
-        }
-
 #if ODIN_INSPECTOR
         [ShowInInspector, ShowIf("showOdinInfo"), ListDrawerSettings(IsReadOnly = true)]
 #endif
-        private readonly List<TimerTask> m_TaskList = new List<TimerTask>();
-        private readonly List<TimerTask> m_ToBeRemoved = new List<TimerTask>();
+        private readonly List<ITimerTask> m_TaskList = new List<ITimerTask>();
+        private readonly List<ITimerTask> m_ToBeRemoved = new List<ITimerTask>();
 
         public float DeltaTime { get; private set; }
 
@@ -131,7 +43,7 @@ namespace PureMVCFramework
             DeltaTime = delta;
             for (int i = 0; i < m_TaskList.Count; ++i)
             {
-                TimerTask task = m_TaskList[i];
+                ITimerTask task = m_TaskList[i];
                 if (task.Update(delta) && task.Execute())
                 {
                     task.Stop();
@@ -144,7 +56,6 @@ namespace PureMVCFramework
                 for (int i = 0; i < m_ToBeRemoved.Count; ++i)
                 {
                     m_TaskList.Remove(m_ToBeRemoved[i]);
-                    ReferencePool.RecycleInstance(m_ToBeRemoved[i]);
                 }
                 m_ToBeRemoved.Clear();
             }
@@ -155,7 +66,7 @@ namespace PureMVCFramework
         /// </summary>
         /// <param name="startDelay">Start delay.</param>
         /// <param name="executable">Executable.</param>
-        public TimerTask AddOneShotTask(float startDelay, Action executable, string taskName = null)
+        public ITimerTask AddOneShotTask(float startDelay, Action executable, string taskName = null)
         {
             return AddTask(TimerType.FIXED_DURATION, Time.time + startDelay, 0, 1, () =>
             {
@@ -169,7 +80,7 @@ namespace PureMVCFramework
         /// </summary>
         /// <param name="startDelay">Start delay.</param>
         /// <param name="executable">Executable.</param>
-        public TimerTask AddRealtimeOneShotTask(float startDelay, Action executable, string taskName = null)
+        public ITimerTask AddRealtimeOneShotTask(float startDelay, Action executable, string taskName = null)
         {
             return AddTask(TimerType.FIXED_REALTIME_DURATION, Time.realtimeSinceStartup + startDelay, 0, 1, () =>
             {
@@ -185,7 +96,7 @@ namespace PureMVCFramework
         /// <param name="interval">Interval.</param>
         /// <param name="repeatTimes">Repeat times, -1 means always.</param>
         /// <param name="executable">Executable.</param>
-        public TimerTask AddRepeatTask(float startDelay, float interval, int repeatTimes, Func<bool> executable, string taskName = null)
+        public ITimerTask AddRepeatTask(float startDelay, float interval, int repeatTimes, Func<bool> executable, string taskName = null)
         {
             return AddTask(TimerType.FIXED_DURATION, Time.time + startDelay, interval, repeatTimes, executable, taskName);
         }
@@ -197,7 +108,7 @@ namespace PureMVCFramework
         /// <param name="interval">Interval.</param>
         /// <param name="repeatTimes">Repeat times, -1 means always.</param>
         /// <param name="executable">Executable.</param>
-        public TimerTask AddRealtimeRepeatTask(float startDelay, float interval, int repeatTimes, Func<bool> executable, string taskName = null)
+        public ITimerTask AddRealtimeRepeatTask(float startDelay, float interval, int repeatTimes, Func<bool> executable, string taskName = null)
         {
             return AddTask(TimerType.FIXED_REALTIME_DURATION, Time.realtimeSinceStartup + startDelay, interval, repeatTimes, executable, taskName);
         }
@@ -206,7 +117,7 @@ namespace PureMVCFramework
         /// Adds the frame execute task.
         /// </summary>
         /// <param name="executable">Executable.</param>
-        public TimerTask AddFrameExecuteTask(Func<bool> executable, string taskName = null)
+        public ITimerTask AddFrameExecuteTask(Func<bool> executable, string taskName = null)
         {
             return AddTask(TimerType.EVERY_FRAME, Time.time, 0, 0, executable, taskName);
         }
@@ -220,19 +131,20 @@ namespace PureMVCFramework
         /// <param name="interval">Interval.</param>
         /// <param name="repeatTimes">Repeat times.</param>
         /// <param name="executable">Executable.</param>
-        private TimerTask AddTask(TimerType timerType, float startTime, float interval, int repeatTimes, Func<bool> executable, string taskName)
+        private ITimerTask AddTask(TimerType timerType, float startTime, float interval, int repeatTimes, Func<bool> executable, string taskName)
         {
-            TimerTask task = ReferencePool.SpawnInstance<TimerTask>();
-            task.timerType = timerType;
-            task.startTime = startTime;
-            task.interval = interval;
-            task.repeatTimes = repeatTimes;
-            task.executable = executable;
+            var task = new TimerTask
+            {
+                timerType = timerType,
+                startTime = startTime,
+                interval = interval,
+                repeatTimes = repeatTimes,
+                executable = executable,
 
-            // TimerTask task = new TimerTask { timerType = timerType, startTime = startTime, interval = interval, repeatTimes = repeatTimes, executable = executable };
 #if UNITY_EDITOR
-            task.name = string.IsNullOrEmpty(taskName) ? DefaultTaskName(executable) : taskName;
+                name = string.IsNullOrEmpty(taskName) ? DefaultTaskName(executable) : taskName,
 #endif
+            };
             m_TaskList.Add(task);
 
             return task;
@@ -246,5 +158,97 @@ namespace PureMVCFramework
             return methodInfo.DeclaringType.Name + "." + methodInfo.Name;
         }
 #endif
+    }
+
+    internal struct TimerTask : ITimerTask, IDisposable
+    {
+        public string name;
+
+        public TimerType timerType;
+        public float startTime;
+        public float interval;
+        public int repeatTimes;
+        public Func<bool> executable;
+
+        private int executeTimes;
+        private float elapsedTime;
+        private float nextExecuteTime;
+        private bool started;
+        private bool stopped;
+
+        public bool IsStopped { get => stopped; }
+
+#if ODIN_INSPECTOR
+        [ShowInInspector]
+#endif
+        public float ElapsedTime => elapsedTime;
+
+        public void Stop()
+        {
+            stopped = true;
+        }
+
+        public void Dispose()
+        {
+            executeTimes = 0;
+            elapsedTime = 0;
+            nextExecuteTime = 0;
+            started = false;
+            stopped = false;
+        }
+
+        public bool Update(float delta)
+        {
+            elapsedTime += delta;
+
+            float time = Time.time;
+            if (timerType == TimerType.FIXED_REALTIME_DURATION)
+                time = Time.realtimeSinceStartup;
+
+            if (!started)
+            {
+                if (time >= startTime)
+                {
+                    started = true;
+                    nextExecuteTime = startTime + interval;
+                    return true;
+                }
+            }
+            else
+            {
+                if (timerType == TimerType.EVERY_FRAME)
+                    return true;
+
+                if (time >= nextExecuteTime)
+                {
+                    nextExecuteTime += interval;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Execute this instance.
+        /// </summary>
+        /// <returns>if <see langword="true"/>, remove this task</returns>
+        public bool Execute()
+        {
+            executeTimes++;
+            if (executable != null)
+            {
+                try
+                {
+                    return stopped || executable() || executeTimes == repeatTimes;
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
+            }
+
+            return true;
+        }
     }
 }
